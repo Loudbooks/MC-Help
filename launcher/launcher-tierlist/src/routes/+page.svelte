9cabac6327275eed2fecb963a6f0098810923885
Loudbook <loudbooked@gmail.com>
2024-08-05 10:34:35 -0400

    Colors

--- launcher/launcher-tierlist/src/routes/+page.svelte
+++ launcher/launcher-tierlist/src/routes/+page.svelte
@@ -30,7 +30,7 @@
         <div id="title-container" class="title">
             <h1 id="title-1" class="title">S</h1>
         </div>
-        <div id="icons-1" class="group">
+        <div id="icons-1" class="group icon-group">
             <LauncherIcon iconName="modrinth" title="Modrinth" link="https://modrinth.app/" color="#1CD96A" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="prism" title="Prism" link="https://prismlauncher.org/" color="#99CD61" description="Expandable, performant, simple, open source. This launcher is bare bones, while still allowing access to all mods ever created."/>
         </div>
@@ -39,7 +39,7 @@
         <div id="title-container" class="title">
             <h1 id="title-2" class="title">A</h1>
         </div>        
-        <div id="icons-2" class="group">
+        <div id="icons-2" class="group icon-group">
             <LauncherIcon iconName="gdlauncher" title="GDLauncher" link="https://minecraft.net/" color="#375076" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="multimc" title="MultiMC" link="https://multimc.org/" color="#7b5437" description="Expandable, performant, simple, open source. This launcher is bare bones, while still allowing access to all mods ever created."/>
             <LauncherIcon iconName="atlauncher" title="ATLauncher" link="https://multimc.org/" color="#89c236" description="Expandable, performant, simple, open source. This launcher is bare bones, while still allowing access to all mods ever created."/>
@@ -50,7 +50,7 @@
         <div id="title-container" class="title">
             <h1 id="title-3" class="title">B</h1>
         </div>
-        <div id="icons-3" class="group">
+        <div id="icons-3" class="group icon-group">
             <LauncherIcon iconName="technic" title="Technic" link="https://prismlauncher.org/" color="#1389d2" description="Expandable, performant, simple, open source. This launcher is bare bones, while still allowing access to all mods ever created."/>
             <LauncherIcon iconName="curseforge" title="CurseForge" link="https://curseforge.com/" color="#f16436" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
         </div>
@@ -59,7 +59,7 @@
         <div id="title-container" class="title">
             <h1 id="title-4" class="title">C</h1>
         </div>
-        <div id="icons-4" class="group">
+        <div id="icons-4" class="group icon-group">
             <LauncherIcon iconName="featherclient" title="Feather" link="https://minecraft.net/" color="#132938" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="badlionclient" title="Badlion" link="https://badlion.net/" color="#2caddc" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="lunarclient" title="Lunar" link="https://badlion.net/" color="#616161" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
@@ -69,7 +69,7 @@
         <div id="title-container" class="title">
             <h1 id="title-5" class="title">F</h1>
         </div>
-        <div id="icons-5" class="group">
+        <div id="icons-5" class="group icon-group">
             <LauncherIcon iconName="tlauncher" title="TLauncher" link="https://minecraft.net/" color="#38a5d5" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="sklauncher" title="SKLauncher" link="https://minecraft.net/" color="#3b4999" description="With developers being well paid, an easy to use interface, and being completely ad free, Modrinth is one of the best launchers you could use."/>
             <LauncherIcon iconName="easymc" title="EasyMC" link="https://prismlauncher.org/" color="#14826c" description="Expandable, performant, simple, open source. This launcher is bare bones, while still allowing access to all mods ever created."/>
@@ -96,7 +96,8 @@
         padding: 20px;
         border-radius: 70px;
         width: 100%;
-        overflow: scroll;
+        // overflow: scroll;
+        // flex: 0 0 auto;
 
         @media (max-width: 600px) {
             flex-wrap: wrap;
@@ -120,13 +121,14 @@
     }
 
     #icons-1 {
-        border: 10px solid gold;
-        background-color: gold;
+        border: 10px solid lightcoral;
+        background-color: lightcoral;
+
     }
 
     #icons-2 {
-        border: 10px solid lightcoral;
-        background-color: lightcoral;
+        border: 10px solid sandybrown;
+        background-color: sandybrown;
     }
 
     #icons-3 {
@@ -140,8 +142,8 @@
     }
 
     #icons-5 {
-        border: 10px solid purple;
-        background-color: purple;
+        border: 10px solid orchid;
+        background-color: orchid;
     }
 
     h1 {
@@ -162,11 +164,11 @@
     }
 
     #title-1 {
-        color: gold;
+        color: lightcoral;
     }
 
     #title-2 {
-        color: lightcoral;
+        color: sandybrown;
     }
 
     #title-3 {
@@ -178,6 +180,6 @@
     }
 
     #title-5 {
-        color: purple;
+        color: orchid;
     }
 </style>
